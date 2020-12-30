ac5c78b3ddc7a85d79467d95019a93c0c47c0677
Alexandre <alexandrejeronimo2001@gmail.com>
2020-12-30 23:50:41 +0000

    New branch

--- main.c
+++ main.c
@@ -12,12 +12,13 @@ int main() {
 
 void MenuPrincipal() {
   int opcao;
-
-  printf("GESTOR DE AULAS ONLINE\n\n");
-  printf("1. Gerir UCs\n");
-  printf("2. Gerir Aulas\n");
-  printf("3. Estatistica\n");
-  opcao = lerInteiro("Opcao", 1, 3);
+  do {
+    printf("GESTOR DE AULAS ONLINE\n\n");
+    printf("1. Gerir UCs\n");
+    printf("2. Gerir Aulas\n");
+    printf("3. Estatistica\n");
+    opcao = lerInteiro("Opcao", 1, 3);
+  } while (opcao < 1 || opcao > 3);
 
   switch (opcao) {
     case 1:
@@ -36,7 +37,6 @@ void MenuPrincipal() {
 
 void MenuUcs() {
   int opcao;
-
   printf("\nGESTOR DE UCs\n");
   printf("1. Registar UC\n");
   printf("2. Editar UC\n");
