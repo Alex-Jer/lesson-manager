3d7c8ab77c7d34cea2f407f5fab1bd08f526f43c
Alexandre <alexandrejeronimo2001@gmail.com>
2020-12-30 23:42:14 +0000

    Initial commit

---
+++ include/funcoes_menu.h
@@ -0,0 +1,7 @@
+#ifndef FUNCOES_MENU_H_INCLUDED
+#define FUNCOES_MENU_H_INCLUDED
+
+void MenuPrincipal();
+void MenuUcs();
+
+#endif /* FUNCOES_MENU_H_INCLUDED */
---
+++ src/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "constantes.h"
+#include "funcoes_auxiliares.h"
+#include "funcoes_menu.h"
+
+int main() {
+  MenuPrincipal();
+  return 0;
+}
+
+void MenuPrincipal() {
+  int opcao;
+
+  printf("GESTOR DE AULAS ONLINE\n\n");
+  printf("1. Gerir UCs\n");
+  printf("2. Gerir Aulas\n");
+  printf("3. Estatistica\n");
+  opcao = lerInteiro("Opcao", 1, 3);
+
+  switch (opcao) {
+    case 1:
+      MenuUcs();
+      break;
+    case 2:
+      break;
+    case 3:
+      break;
+
+    default:
+      printf("0");
+      break;
+  }
+}
+
+void MenuUcs() {
+  int opcao;
+
+  printf("\nGESTOR DE UCs\n");
+  printf("1. Registar UC\n");
+  printf("2. Editar UC\n");
+  printf("3. Listar UCs\n");
+  opcao = lerInteiro("Opcao", 1, 3);
+}
