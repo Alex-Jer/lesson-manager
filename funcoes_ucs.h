c46e71a1c2f6c728a914970353845fbf0e645984
Alexandre <alexandrejeronimo2001@gmail.com>
2021-01-02 19:07:26 +0000

    Funções UCs e registar UC

---
+++ funcoes_ucs.h
@@ -0,0 +1,6 @@
+#ifndef FUNCOES_UCS_H_INCLUDED
+#define FUNCOES_UCS_H_INCLUDED
+
+UnidadeCurricular RegistarUc();
+
+#endif /* FUNCOES_UCS_H_INCLUDED */
--- main.c
+++ main.c
@@ -2,23 +2,24 @@
 #include <stdlib.h>
 
 #include "constantes.h"
+#include "estruturas.h"
 #include "funcoes_auxiliares.h"
 #include "funcoes_menu.h"
+#include "funcoes_ucs.h"
 
 int main() {
-  MenuPrincipal();//teste
+  MenuPrincipal();
   return 0;
 }
 
 void MenuPrincipal() {
   int opcao;
-  do {
-    printf("GESTOR DE AULAS ONLINE\n\n");
-    printf("1. Gerir UCs\n");
-    printf("2. Gerir Aulas\n");
-    printf("3. Estatistica\n");
-    opcao = lerInteiro("Opcao", 1, 3);
-  } while (opcao < 1 || opcao > 3);
+
+  printf("GESTOR DE AULAS ONLINE\n\n");
+  printf("1. Gerir UCs\n");
+  printf("2. Gerir Aulas\n");
+  printf("3. Estatistica\n");
+  opcao = LerInteiro("Opcao", 1, 3);
 
   switch (opcao) {
     case 1:
@@ -37,9 +38,23 @@ void MenuPrincipal() {
 
 void MenuUcs() {
   int opcao;
+  UnidadeCurricular ucs[MAX_UCS];
+
   printf("\nGESTOR DE UCs\n");
   printf("1. Registar UC\n");
   printf("2. Editar UC\n");
   printf("3. Listar UCs\n");
-  opcao = lerInteiro("Opcao", 1, 3);
+  opcao = LerInteiro("Opcao", 1, 3);
+
+  switch (opcao) {
+    case 1:
+      ucs[0] = RegistarUc();
+      break;
+    case 2:
+      break;
+    case 3:
+      break;
+    default:
+      break;
+  }
 }
