da213bf6a1df322e16f0b45b9096bad817a80bd8
Madrux <joaolmadruga@gmail.com>
2021-01-04 17:20:39 +0000

    teste

---
+++ funcoes_aula.h
@@ -0,0 +1,10 @@
+#ifndef FUNCOES_AULA_H_INCLUDED
+#define FUNCOES_AULA_H_INCLUDED
+
+
+UnidadeCurricular RegistarAula();
+
+#endif
+
+
+#endif
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #include "funcoes_auxiliares.h"
 #include "funcoes_menu.h"
 #include "funcoes_ucs.h"
-
+#include "funcoes_aula.h"
 int main() {
   MenuPrincipal();
   return 0;
@@ -23,9 +23,10 @@ void MenuPrincipal() {
 
   switch (opcao) {
     case 1:
-      MenuUcs();
+        MenuUcs();
       break;
     case 2:
+           MenuAula();
       break;
     case 3:
       break;
@@ -49,6 +50,28 @@ void MenuUcs() {
   switch (opcao) {
     case 1:
       ucs[0] = RegistarUc();
+      break;
+    case 2:
+      break;
+    case 3:
+      break;
+    default:
+      break;
+  }
+}
+void MenuAula() {
+  int opcao;
+  UnidadeCurricular [MAX_UCS];
+
+  printf("\nGESTOR DE Aula\n");
+  printf("1. Registar Aula\n");
+  printf("2. Editar Aula\n");
+  printf("3. Listar Aula\n");
+  opcao = LerInteiro("Opcao", 1, 3);
+
+  switch (opcao) {
+    case 1:
+      aula[0] = RegistarAula();
       break;
     case 2:
       break;
