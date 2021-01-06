1a424443440bad090a0b1a8aabfe40cc6ed47e2c
Madrux <joaolmadruga@gmail.com>
2021-01-06 18:58:29 +0000

    duracao com switch
    Ver se pode ser assim

--- funcoes_ucs.c
+++ funcoes_ucs.c
@@ -8,7 +8,7 @@ UnidadeCurricular RegistarUc() {
   char obrigatoria;
   char diurno;
   char tipoDes[2];
-
+    int duracao=0;
   novaUc.obrigatoria = true;
   novaUc.diurno = true;
 
@@ -31,22 +31,32 @@ UnidadeCurricular RegistarUc() {
   novaUc.num_tipo_aulas_previstas = LerInteiro("Numero de aulas previstas", 1, 40);  //! Usar uma constante
   // TODO: Estrutura (?) T, TP, PL
   printf("Qual o tipo de aula?(T/TP/PL): ");  // Ler Char
-  scanf("%s", &tipoDes);
-  // TODO: Duração de cada T, TP, PL
-
-  LerInteiro("Duracao da aula (Horas): ", novaUc.duracao, MAX_DURACAO);
-  if (novaUc.duracao == 'T' || novaUc.duracao == 'TP') {
-    printf("2 Horas");
-  } else {
-    printf("3 Horas");
+  scanf(" %s",&tipoDes);
+printf("Digite a opção refere há pergunta aterior(1-T/2-TP/3-PL): ");
+scanf("%d",&duracao);
+  switch (duracao)
+  {
+      case 1:
+      {
+          printf("A duração da aula será de 0 min ");
+      }
+      break;
+      case 2:
+      {
+          printf("A duração da aula será de 120 min ");
+      }
+      break;
+      case 3:
+      {
+          printf("A duração da aula será de 180 min ");
+      }
+        break;
   }
+  // TODO: Duração de cada T, TP, PL
 
   // Confirmar se escreve tudo
   printf(
-      "Os dados são:\nNome: %s \n Obrigatoria: %c \n Semestre: %d \nHorario %c \nAulas previstas: %d \nTipo de aula: "
-      "%s \n Duração: %d",
-      novaUc.designacao, obrigatoria, novaUc.semestre, diurno, novaUc.num_tipo_aulas_previstas, tipoDes,
-      novaUc.duracao);
+      "Os dados são:\nNome: %s \n Obrigatoria: %c \n Semestre: %d \nHorario %c \nAulas previstas: %d \nTipo de aula:%s/n %s",novaUc.designacao, obrigatoria, novaUc.semestre, diurno, novaUc.num_tipo_aulas_previstas, tipoDes,duracao);
 
   return novaUc;
 }
